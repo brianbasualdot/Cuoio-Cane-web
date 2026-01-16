89724bc5d35576f1bb7faf58849536adccdd1c07
brianbasualdot <brianbasualdot@gmail.com>
2026-01-16 17:53:10 -0300

    feat: add PaymentsModule with its service, controller, and necessary module imports.

--- apps/api/src/modules/payments/payments.module.ts
+++ apps/api/src/modules/payments/payments.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PaymentsService } from './payments.service';
 import { PaymentsController } from './payments.controller';
+import { ConfigModule } from '@nestjs/config';
+import { DiscountsModule } from '../discounts/discounts.module';
 
 @Module({
+    imports: [ConfigModule, DiscountsModule],
     controllers: [PaymentsController],
     providers: [PaymentsService],
 })
